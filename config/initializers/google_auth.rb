881178466fb1c7d9d3b38613d26f91f71e883110
pranav-137 <pranavkumar12384@gmail.com>
2025-11-08 10:26:37 +0530

    gallery and google auth resolve

--- config/initializers/google_auth.rb
+++ config/initializers/google_auth.rb
@@ -2,7 +2,14 @@ require 'googleauth'
 require 'google/apis/gmail_v1'
 require 'googleauth/stores/file_token_store'  # 👈 THIS LINE IS MANDATORY
 
-GMAIL_SCOPE = 'https://www.googleapis.com/auth/gmail.send'
-GOOGLE_CLIENT_ID = Google::Auth::ClientId.from_file(Rails.root.join('client_secret.json'))
-TOKEN_STORE = Google::Auth::Stores::FileTokenStore.new(file: Rails.root.join('token.yaml'))
-GOOGLE_AUTHORIZER = Google::Auth::UserAuthorizer.new(GOOGLE_CLIENT_ID, GMAIL_SCOPE, TOKEN_STORE)
+if Rails.env.development? || Rails.env.production?
+  secret_file = Rails.root.join('client_secret.json')
+  if File.exist?(secret_file)
+    GOOGLE_CLIENT_ID = Google::Auth::ClientId.from_file(secret_file)
+    GOOGLE_TOKEN_STORE = Google::Auth::Stores::FileTokenStore.new(file: 'tokens.yaml')
+    GMAIL_SCOPE = 'https://www.googleapis.com/auth/gmail.send'
+    GOOGLE_AUTHORIZER = Google::Auth::UserAuthorizer.new(GOOGLE_CLIENT_ID, GMAIL_SCOPE, GOOGLE_TOKEN_STORE)
+  else
+    Rails.logger.warn("⚠️ Google client_secret.json missing, skipping Google Auth setup.")
+  end
+end
